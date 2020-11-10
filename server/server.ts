79dd3dfae694bc9b8432049209ea7e71c7b23641
Thiago Henrique dos Santos <henrique.thsantos.ths@gmail.com>
2020-11-10 15:55:46 -0300

    DB connection added

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,17 @@
 import * as restify from 'restify';
 import { environment } from '../common/environment';
 import { Router } from '../common/router';
+import * as mongoose from 'mongoose';
 
 export class Server {
     application: restify.Server;
+
+    initializeDB(): mongoose.MongooseThenable {
+        (<any>mongoose).Promise = global.Promise;
+        return mongoose.connect(environment.db.url, {
+            useMongoClient: true,
+        });
+    }
 
     initRoutes(routers: Router[]): Promise<any> {
         return new Promise((resolve, reject) => {
@@ -40,7 +48,9 @@ export class Server {
         });
     }
 
-    bootstrap(routers: Router[]) : Promise<Server> {
-        return this.initRoutes(routers).then(() => this);
+    bootstrap(routers: Router[]): Promise<Server> {
+        return this.initializeDB().then(() =>
+            this.initRoutes(routers).then(() => this)
+        );
     }
 }
